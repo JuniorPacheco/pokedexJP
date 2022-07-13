a4032f3c21b8b5f2c9d285fc1eeb224b9ab27cc7
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-07-13 17:07:06 -0500

    corrigiendo cambios de performance y boton

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,16 @@ import { Outlet, Navigate } from 'react-router-dom'
 import IconPokeball from "./IconPokeball"
 import { useSelector } from 'react-redux/es/hooks/useSelector'
 import Logo from '../images/logo.png'
+import { useDispatch } from 'react-redux'
+import { setTrainerName } from '../store/slices/trainerName.slice'
 
 const Layout = () => {
+    const dispatch = useDispatch()
     const trainerName = useSelector(state => state.trainerName)
 
+    const goToLogout = () => {
+        dispatch(setTrainerName(''))
+    }
 
   return (
     <>
@@ -19,6 +25,9 @@ const Layout = () => {
                 </figure>
             </section>
             <section className="header__blackBlock">
+                <button onClick={goToLogout} className='button__logout'>
+                    <i className='bx bx-log-out'></i>
+                </button>
                 <IconPokeball pokeballLayout={'pokeballLayout'}/>
             </section>
         </header>
